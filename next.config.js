a0773320919ca3f1690c951c29e620ddc1436da0
Derkysan <derkysan19@gmail.com>
2023-07-13 00:47:09 -0400

    fix ui

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export',
+  output: 'export',
   // path: 'https://avatars.githubusercontent.com/',
   images: {
     unoptimized: true
