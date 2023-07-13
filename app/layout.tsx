0b0b3e080bbac3a49a18c6f5076adfa8a39dc90f
Derkysan <derkysan19@gmail.com>
2023-07-13 01:17:39 -0400

    fix deeplinking

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,25 @@ export const metadata = {
   title: 'Derkysan',
   description: 'Front-end developer',
   openGraph: {
-    images: '/og-image.png',
+    title: 'Next.js',
+    description: 'The React Framework for the Web',
+    url: 'https://nextjs.org',
+    siteName: 'Next.js',
+    images: [
+      {
+        url: 'https://nextjs.org/og.png',
+        width: 800,
+        height: 600,
+      },
+      {
+        url: 'https://nextjs.org/og-alt.png',
+        width: 1800,
+        height: 1600,
+        alt: 'My custom alt',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
   },
 }
 
