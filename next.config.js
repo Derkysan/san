32dfcbdd72c3d2a5444d164205e45d1b647b2840
Derkysan <derkysan19@gmail.com>
2023-08-02 13:15:01 -0400

    fixing navigation

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // basePath: '/san',
+  basePath: '/san',
   // path: 'https://avatars.githubusercontent.com/',
   images: {
     unoptimized: true
